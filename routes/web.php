4df3fae6714ac77c3103321c665836c35358705c
M-Hidayatullah <hidayatullahblk@gmail.com>
2024-02-17 20:47:14 +0800

    layouting

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
